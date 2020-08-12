b60b39b064224dce6dc20e18a53a4aa650aff3a7
krzysztof <misiek.nalepa@wp.pl>
2020-08-13 00:10:35 +0200

    load more button added

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -7,29 +7,53 @@ class GamesList extends React.Component {
         this.state = {
             games: [],
             currentPage: null,
-            totalPages: null
+            totalPages: null,
+            btnState: "visible"
         }
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/api/v1/games?page=0&size=8")
+        fetch("http://localhost:8080/api/v1/games?page=0&size=18")
             .then(response => response.json())
             .then(data => this.setState({
                 games: data.content,
                 currentPage: 1,
-                totalPages: data.totalPages
+                totalPages: data.totalPages,
+                btnState: "visible"
             }))
     }
 
+    handleClick() {
+        fetch(`http://localhost:8080/api/v1/games?page=${this.state.currentPage}&size=18`)
+            .then(response => response.json())
+            .then(data => this.setState(previousState => {
+                return {
+                    games: previousState.games.concat(data.content),
+                    currentPage: previousState.currentPage+1,
+                    totalPages: data.totalPages,
+                    btnState: previousState.currentPage+1 === data.totalPages ? "hidden" : "visible"
+                }
+            }))
+            .then(() => console.log(this.state))
+    }
+
+
     render () {
         const htmlList = this.state.games.map(game =>
-                <GamesListElement name={game.name} imageUrl={game.imageUrl} price={game.price}/>
+                <GamesListElement name={game.name} imageUrl={game.imageUrl} price={game.price} id={game.gameId}/>
             );
 
         return(
             <div className="container mt-4">
                 <div className="row">
                     {htmlList}
+                </div>
+
+                <div style={{textAlign: "center"}}>
+                    <button type="button" className= "btn btn-primary button-standard" style={{visibility: this.state.btnState}}
+                            onClick={this.handleClick}>Więcej</button>
                 </div>
 
             </div>
