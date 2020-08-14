192f3a938a8797ff32b33dd978087e774a9965a5
krzysztof <misiek.nalepa@wp.pl>
2020-08-14 19:41:30 +0200

    karuzellla

---
+++ src/components/Banner.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
+import banner1 from "../img/banner1.jpg"
+import banner2 from "../img/banner2.jpg"
+import banner3 from "../img/banner3.jpg"
+
+class Banner extends React.Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Third slide"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
+
+export default Banner
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,25 +1,27 @@
 import React from "react";
 import BestSellersList from "./BestSellersList";
 import {Link} from "react-router-dom";
+import Banner from "./Banner";
 
 class LandingPage extends React.Component {
     render() {
         return(
+
             <div className="container console-pick-container " style={{textAlign: "center"}}>
+                <div className="row d-none d-sm-block mb-4">
+                    <Banner />
+                </div>
+
                 <h2>Wybierz swoją konsolę</h2>
                 <div className="row console-pick mt-5 mb-5" >
-
-
-
-
-                    <div className="col-6" style={{display: "flex", justifyContent: "flex-end"}}>
+                    <div className="col-12 col-md-2 offset-md-4">
                         <Link to="/shop/playstation_4">
                             <img src="https://image.flaticon.com/icons/svg/588/588258.svg" />
                         </Link>
                     </div>
 
 
-                    <div className="col-6"  style={{display: "flex", justifyContent: "flex-start"}}>
+                    <div className="col-12 col-md-1">
                         <Link to="/shop/xbox_one">
                             <img src="https://image.flaticon.com/icons/svg/732/732260.svg" />
                         </Link>
