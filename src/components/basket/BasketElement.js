37893d8f23bf74dcca4d8e4b6d33a7744de881b6
krzysztof <misiek.nalepa@wp.pl>
2020-09-16 18:48:55 +0200

    color update

--- src/components/basket/BasketElement.js
+++ src/components/basket/BasketElement.js
@@ -9,7 +9,7 @@ class BasketElement extends React.Component {
         return (
             <tr >
                 <td><img src={this.props.game.imageUrl} width={80}/></td>
-                <td ><h4>{this.props.game.name}</h4></td>
+                <td ><h4 style={{color: "#3631C2"}}>{this.props.game.name}</h4></td>
                 <td className="text-center">{this.props.game.price} zł</td>
                 <td className="text-right">
                     <button className="btn btn-sm btn-danger">Usuń</button>
